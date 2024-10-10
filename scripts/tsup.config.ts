38aa63b4683c023032306a6e5f57d37cac0d152b
7TeamApp <zhyceg@gmail.com>
2024-10-10 11:46:17 +0300

    posttsup

--- scripts/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Options } from 'tsup';
-
-export default (<Options>{
-    entryPoints: ['scripts/**/!(tsup.config.ts)'],
-    format: ['esm'],
-    dts: false,
-    outDir: 'dist',
-    external: ['bun','chalk', 'fast-glob', /^node:/, 'path', 'url', /.*node$/]
-});
