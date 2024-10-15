22ce4dee5b72d696dd64f3fb488555d7b3ee9b31
7TeamApp <zhyceg@gmail.com>
2024-10-16 00:38:46 +0300

    test

--- scripts/tester.cjs
+++ scripts/tester.cjs
@@ -9,69 +9,97 @@ function stdout(err, stdout, stderr) {
     console.log(stdout);
 }
 
-function isComExist(cmd) {
-    return new Promise(function (res) {
-        cp.exec(cmd + ' --help', function (err) {
+function ifComExist(tester, fnTrue, fnFalse) {
+    new Promise(function (res) {
+        cp.exec(tester + ' --help', function (err) {
             res(!err);
         });
+    }).then(function (res) {
+        if (res) {
+            console.log('using ' + tester);
+            fnTrue();
+        } else {
+            fnFalse();
+        }
     });
 }
 
-isComExist('bun').then(function (res) {
-    if (res) {
-        console.log('using bun');
-        cp.exec('bun test .ts', stdout);
-    } else {
-        isComExist('pnpm').then(function (res) {
-            if (res) {
-                console.log('using pnpm');
-                isComExist('pnpm vitest').then(function (res) {
-                    if (res) {
-                        cp.exec('pnpm vitest run --dir test', stdout);
-                    } else {
-                        cp.exec(
-                            "pnpm jest --rootDir test_dist --testMatch '**/*.cjs'",
-                            stdout
-                        );
-                    }
-                });
-            } else {
-                isComExist('yarn').then(function (res) {
-                    if (res) {
-                        console.log('using yarn');
-                        isComExist('yarn vitest').then(
-                            function (res) {
-                                if (res) {
-                                    cp.exec(
-                                        'yarn vitest run --dir test',
-                                        stdout
-                                    );
-                                } else {
-                                    cp.exec(
-                                        "yarn jest --rootDir test_dist --testMatch '**/*.cjs'",
-                                        stdout
-                                    );
-                                }
-                            }
-                        );
-                    } else {
-                        console.log('using npm');
-                        isComExist('npx vitest').then(function (res) {
-                            if (res) {
-                                cp.exec(
-                                    'npx vitest run --dir test',
-                                    stdout
-                                );
-                            } else {
-                                cp.exec(
-                                    "npx jest --rootDir test_dist --testMatch '**/*.cjs'",
-                                    stdout
-                                );
-                            }
-                        });
-                    }
-                });
-            }
-        });
+function std(tester) {
+    switch (tester) {
+        case 'bun': {
+            break;
+        }
+        case 'pnpm': {
+            break;
+        }
+        case 'yarn': {
+            break;
+        }
+        case 'npm': {
+            break;
+        }
+        default: {
+            throw new Error('incorrect tester');
+        }
     }
-});
+}
+
+function exec(cmd) {
+    cp.exec(cmd, stdout);
+}
+
+ifComExist(
+    'bun',
+    function () {
+        exec('bun test .ts');
+    },
+    function () {
+        ifComExist(
+            'pnpm',
+            function () {
+                ifComExist(
+                    'pnpm vitest',
+                    function () {
+                        exec('pnpm vitest run --dir test');
+                    },
+                    function () {
+                        exec(
+                            "pnpm jest --rootDir test_dist --testMatch '**/*.cjs'"
+                        );
+                    }
+                );
+            },
+            function () {
+                ifComExist(
+                    'yarn',
+                    function () {
+                        ifComExist(
+                            'yarn vitest',
+                            function () {
+                                exec('yarn vitest run --dir test');
+                            },
+                            function () {
+                                exec(
+                                    "yarn jest --rootDir test_dist --testMatch '**/*.cjs'"
+                                );
+                            }
+                        );
+                    },
+                    function () {
+                        ifComExist(
+                            'npx vitest',
+                            function () {
+                                cp.exec('npx vitest run --dir test');
+                            },
+                            function () {
+                                cp.exec(
+                                    "npx jest --rootDir test_dist --testMatch '**/*.cjs'"
+                                );
+                            }
+                        );
+                    }
+                );
+            }
+        );
+    }
+);
