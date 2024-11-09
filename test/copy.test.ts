c4810e9a305c6efeca619e58594e938f9e05f517
7TeamApp <zhyceg@gmail.com>
2024-11-10 02:46:25 +0300

    try again

--- test/copy.test.ts
+++ test/copy.test.ts
@@ -2,7 +2,6 @@ import fs from 'node:fs';
 import { join } from 'node:path';
 import { copy } from 'files-rs';
 import { test, expect, beforeEach, afterEach } from 'lib/tester';
-import { console } from 'node:inspector';
 
 const fileName = 'testSrc.txt';
 const srcFile = join(__dirname, fileName);
