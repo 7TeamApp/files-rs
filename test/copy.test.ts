974801fa78e3715076c1f5b2ed7590b8c6470243
7TeamApp <zhyceg@gmail.com>
2024-11-10 02:16:18 +0300

    first test

--- test/copy.test.ts
+++ test/copy.test.ts
@@ -1,49 +1,40 @@
-// import fs from 'node:fs';
-// import { join } from 'node:path';
-// import { copy } from 'files-rs';
-// import tester from '../lib/tester';
+import fs from 'node:fs';
+import { join } from 'node:path';
+import { copy } from 'files-rs';
+import { test, expect, beforeEach, afterEach } from 'lib/tester';
 
-import {test, expect} from 'lib/tester';
+const fileName = 'testSrc.txt';
+const srcFile = join(__dirname, fileName);
+const destFolder = join(__dirname, 'dest');
+const fileConstent = 'tertewqwdwd';
 
-test('copy one file relative', () => {
-    // console.log(2);
-    // copy(srcFile, destFolder);
-    // const destFileContent = fs.readFileSync(
-    //     `${destFolder}/${fileName}`,
-    //     'utf-8'
-    // );
-
-    expect(1).toBe(1);
+beforeEach(async () => {
+    await fs.writeFile(srcFile, fileConstent, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 });
 
-// const fileName = 'testSrc.txt';
-// const srcFile = join(__dirname, fileName);
-// const destFolder = join(__dirname, 'dest');
-// const fileConstent = 'tertewqwdwd';
+test('copy one file relative', () => {
+    copy(srcFile, destFolder);
+    const destFileContent = fs.readFileSync(
+        `${destFolder}/${fileName}`,
+        'utf-8'
+    );
 
-// beforeEach(async () => {
-//     console.log(1);
-//     await fs.writeFile(srcFile, fileConstent, (err) => {
-//         if (err) {
-//             console.error(err);
-//         }
-//     });
-// });
+    expect(fileConstent).toBe(destFileContent);
+});
 
-
-
-// afterEach(async () => {
-//     console.log(3);
-//     await fs.unlink(srcFile, (err) => {
-//         if (err) {
-//             console.error(err);
-//         }
-//     });
-//     await fs.unlink(`${destFolder}/${fileName}`, (err) => {
-//         if (err) {
-//             console.error(err);
-//         }
-//     });
-// });
-
-export {} 
+afterEach(async () => {
+    await fs.unlink(srcFile, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+    await fs.unlink(`${destFolder}/${fileName}`, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
