5a122ca56b9235b125b3cb993f54c45d25bc5d94
7TeamApp <zhyceg@gmail.com>
2024-11-10 05:17:05 +0300

    for vitest

--- test/copy.test.ts
+++ test/copy.test.ts
@@ -2,36 +2,11 @@ import fs from 'node:fs';
 import { join } from 'node:path';
 import crypto from 'node:crypto';
 import { copy } from 'files-rs';
-import {
-    test,
-    expect,
-    jest,
-    beforeEach,
-    afterEach
-} from 'lib/tester';
-
-const getRandText = jest.fn((length: number): string =>
-    crypto.randomBytes(length).toString('hex').slice(0, length)
-);
-
-const getRandNum = jest.fn(
-    (min: number, max: number): number =>
-        Math.floor(Math.random() * (max - min + 1)) + min
-);
-
-const getRandPath = jest.fn(
-    (count: number, min: number, max: number): string[] => {
-        const randStrs: string[] = [];
-        for (let i = 0; i < count; i++) {
-            randStrs.push(getRandText(getRandNum(min, max)));
-        }
-
-        return randStrs;
-    }
-);
+import { test, expect, beforeEach, afterEach } from 'lib/tester';
 
 const distFolder: string = join(__dirname, 'dist');
 const rerunEach: number = 1000;
+const each = new Array(rerunEach).fill(undefined);
 
 let fileName: string;
 let copiedDistFolder: string;
@@ -40,8 +15,30 @@ let copiedFile: string;
 let fileConstent: string;
 
 beforeEach(() => {
+    const getRandText = (length: number): string =>
+        crypto.randomBytes(length).toString('hex').slice(0, length);
+
+    const getRandNum = (min: number, max: number): number =>
+        Math.floor(Math.random() * (max - min + 1)) + min;
+
+    const getRandPath = (
+        count: number,
+        min: number,
+        max: number
+    ): string[] => {
+        const randStrs: string[] = [];
+        for (let i = 0; i < count; i++) {
+            randStrs.push(getRandText(getRandNum(min, max)));
+        }
+
+        return randStrs;
+    };
+
     fileName = getRandText(getRandNum(2, 20));
-    copiedDistFolder = join(distFolder, ...getRandPath(getRandNum(1,9), 2, 20));
+    copiedDistFolder = join(
+        distFolder,
+        ...getRandPath(getRandNum(1, 9), 2, 20)
+    );
     srcFile = join(distFolder, fileName);
     copiedFile = join(copiedDistFolder, fileName);
     fileConstent = getRandText(getRandNum(1, 1000));
@@ -50,9 +47,7 @@ beforeEach(() => {
     fs.writeFileSync(srcFile, fileConstent);
 });
 
-const cases = new Array(rerunEach).fill(undefined);
-
-test.each(cases)('copy one file relative', () => {
+test.each(each)('copy one file relative', () => {
     copy(srcFile, copiedDistFolder);
 
     const destFileContent = fs.readFileSync(copiedFile, 'utf-8');
